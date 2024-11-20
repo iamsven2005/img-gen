d7f5e6592851f43305673a7fee30bde96dd5658f
iamsven2005 <iamsven2005@gmail.com>
2024-11-20 14:59:46 +0800

    done

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -5,7 +5,7 @@ export async function POST(req: Request) {
   const { messages } = await req.json();
 
   const result = await streamText({
-    model: openai("gpt-4o-mini"),
+    model: openai("ft:gpt-4o-mini-2024-07-18:personal:yaml:AVTn6rFa"),
     system:
       "you are a cisco network topology expert, do not respond on markdown or lists, keep your responses brief, you can ask the user to upload images or documents if it could help you understand the problem better, try to return a yaml file where possible",
     messages: convertToCoreMessages(messages),
